dd9b6ba8c7720b87a3b9cf1fcbf735491bac1769
Cellmander <luandebrito.andrade25.2002@gmail.com>
2024-10-08 22:11:55 -0300

    commit

--- resumoExpandido.R
+++ resumoExpandido.R
@@ -5,8 +5,8 @@ summary(dados)
 
 ftabela = table(dados$causa_acidente, useNA = "ifany") 
 ptabela = round(prop.table(ftabela)*100,1)
-tabela1 = data.frame(ftabela,ptabela)
-tabela1 = tabela1[,-3]
+tabela1 <- data.frame(Causa_dos_acidentes = names(ftabela), Frequencia = as.vector(ftabela), Porcentagem = as.vector(ptabela))
+tabela1 <- tabela1[order(-tabela1$Frequencia), ]
 colnames(tabela1) <- c("Causa dos acidentes","Frequencia","Porcentagem") 
 tabela1
 write.table(tabela1,"Tabela.csv", sep=";", dec=",", row.names=FALSE)
@@ -15,7 +15,7 @@ write.table(tabela1,"Tabela.csv", sep=";", dec=",", row.names=FALSE)
 diatabela = table(dados$dia_semana, useNA = "ifany")
 ptabela = round(prop.table(diatabela)*100,1)
 tabela2 = data.frame(diatabela,ptabela)
-tabela2 = tabela2[,-4]
+tabela2 = tabela2[,-3]
 colnames(tabela2) <- c("Dia Da Semana","Frequencia Total","Porcentagem") 
 
 #condicao metereologica
@@ -38,4 +38,22 @@ ptabela = round(prop.table(cidadetabela)*100,1)
 tabela5 = data.frame(cidadetabela,ptabela)
 tabela5 = tabela5[,-3]
 colnames(tabela5) <- c("Municipio","Frequencia Total","Porcentagem") 
+
+#causa de acidente mais frequente
+library(dplyr)
+acidentes_fatais <- dados %>% filter(mortos > 1)
+causa_acidente_fatal <- acidentes_fatais %>%
+  group_by(causa_acidente) %>%
+  summarise(total_mortos = sum(mortos)) %>%
+  arrange(desc(total_mortos))
+
+dados <- dados %>% mutate(acidente_fatal = ifelse(mortos > 0, 1, 0))
+acidentes_por_causa <- dados %>%
+  group_by(causa_acidente) %>%
+  summarise(
+    total_acidentes = n(),
+    acidentes_fatais = sum(acidente_fatal)
+  ) %>%
+  mutate(taxa_mortalidade = acidentes_fatais / total_acidentes * 100) %>%
+  arrange(desc(taxa_mortalidade))
 
